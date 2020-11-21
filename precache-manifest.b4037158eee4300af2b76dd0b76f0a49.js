64483d2b42c913f54f6deea49d2de7e96d4bc2e5
boyarkinev <boyarkinev@MacBook-Pro-total.local>
2020-11-21 15:20:43 +0300

    Updates

---
+++ precache-manifest.b4037158eee4300af2b76dd0b76f0a49.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4438a84e7510e9b9f3158ab523774ec4",
+    "url": "/dadachat/index.html"
+  },
+  {
+    "revision": "6bfe8962f91cf09455ab",
+    "url": "/dadachat/static/css/3.659fab35.chunk.css"
+  },
+  {
+    "revision": "97d26cf6179d4c712d56",
+    "url": "/dadachat/static/css/4.c30a5491.chunk.css"
+  },
+  {
+    "revision": "fbafc9b48edbf38687f4",
+    "url": "/dadachat/static/css/5.829d2d1d.chunk.css"
+  },
+  {
+    "revision": "b8abb9c1629cff8dbd50",
+    "url": "/dadachat/static/css/main.c9310753.chunk.css"
+  },
+  {
+    "revision": "59e30474af96e094dd6d",
+    "url": "/dadachat/static/js/2.27743494.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/dadachat/static/js/2.27743494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfe8962f91cf09455ab",
+    "url": "/dadachat/static/js/3.ba57a49b.chunk.js"
+  },
+  {
+    "revision": "97d26cf6179d4c712d56",
+    "url": "/dadachat/static/js/4.cee8e802.chunk.js"
+  },
+  {
+    "revision": "fbafc9b48edbf38687f4",
+    "url": "/dadachat/static/js/5.1d53400c.chunk.js"
+  },
+  {
+    "revision": "a67a0c24e41e8872e524",
+    "url": "/dadachat/static/js/6.1aa6573b.chunk.js"
+  },
+  {
+    "revision": "b8abb9c1629cff8dbd50",
+    "url": "/dadachat/static/js/main.b47e9790.chunk.js"
+  },
+  {
+    "revision": "58ac4863263377682047",
+    "url": "/dadachat/static/js/runtime-main.17a47cee.js"
+  },
+  {
+    "revision": "cc571328d34ebae014f1783d5372f30d",
+    "url": "/dadachat/static/media/cover_pic_elem_1.cc571328.svg"
+  },
+  {
+    "revision": "58541e94ae826990909661b10131da92",
+    "url": "/dadachat/static/media/cover_pic_elem_2.58541e94.svg"
+  },
+  {
+    "revision": "0c2b72f20c075985a4a2829bbef809d9",
+    "url": "/dadachat/static/media/cover_pic_elem_3.0c2b72f2.svg"
+  },
+  {
+    "revision": "34a6af410496d74ef34d8856f341e7bf",
+    "url": "/dadachat/static/media/pacifico.34a6af41.woff"
+  },
+  {
+    "revision": "9284e497806e0d46ddb3ed9ca9889d03",
+    "url": "/dadachat/static/media/pacifico.9284e497.woff2"
+  },
+  {
+    "revision": "763f39e584548fffd56c00064ddf9b73",
+    "url": "/dadachat/static/media/preloader.763f39e5.png"
+  },
+  {
+    "revision": "0b11ae5394155ac106bcb741b5238969",
+    "url": "/dadachat/static/media/roboto-medium.0b11ae53.woff2"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/dadachat/static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/dadachat/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/dadachat/static/media/roboto-regular.9feb0110.woff2"
+  },
+  {
+    "revision": "23a289800b587c43946b51a0ca877dd8",
+    "url": "/dadachat/static/media/robotoslab-bold.23a28980.woff2"
+  },
+  {
+    "revision": "d63ef23299458362f3edbf6cd8c2c510",
+    "url": "/dadachat/static/media/robotoslab-bold.d63ef232.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/dadachat/static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "935f26736a5e142c5f68c5cb6821c185",
+    "url": "/dadachat/static/media/robotoslab-regular.935f2673.woff2"
+  },
+  {
+    "revision": "e9fbda13db272069f408ebe6726a9ae2",
+    "url": "/dadachat/static/media/userpic.e9fbda13.svg"
+  }
+]);
